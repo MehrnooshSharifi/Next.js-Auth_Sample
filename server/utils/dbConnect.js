9a939652211b994f9cf3c4e0a4893133202b940a
Mehrnoosh <me88.sharifi@gmail.com>
2023-02-25 16:36:00 +0330

    change connection config

--- server/utils/dbConnect.js
+++ server/utils/dbConnect.js
@@ -5,7 +5,7 @@ async function dbConnect() {
   if (connection.isConnected) {
     return;
   }
-  const db = await mongoose.connect(process.env.MONGO_URI, {
+  const db = await mongoose.connect("process.env.MONGO_URI", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
